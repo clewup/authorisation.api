599217cbf6b87c7a975acba1bb38c2c910496034
clew <lewis@clewup.co.uk>
2022-11-16 15:11:43 +0000

    Updated DbConfig to correct credentials.

--- authorisation.api/Data/DbConfig.cs
+++ authorisation.api/Data/DbConfig.cs
@@ -2,8 +2,8 @@ namespace authorisation.api.Data;
 
 public class DbConfig
 {
-    public string ConnectionString = "mongodb+srv://clew:gr7vQS5DP3VGHiFN@clew-network.civ8lz0.mongodb.net/?retryWrites=true&w=majority";
-    public string DatabaseName = "bug_tracker";
+    public string ConnectionString = "mongodb+srv://DUMMYDB:Fn4pxRlgLt4jtDL8TxnOn3UKLQ133dRK@authorisation.rh3pjqv.mongodb.net/?retryWrites=true&w=majority";
+    public string DatabaseName = "authorisation";
     public string UserCollectionName = "users";
     public string RoleCollectionName = "roles";
 }
