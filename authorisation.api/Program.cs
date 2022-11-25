b1a0110718a63077babc0afd969f7a26d5438905
clew <lewis@clewup.co.uk>
2022-11-25 22:49:09 +0000

    Updated CORs policy.

--- authorisation.api/Program.cs
+++ authorisation.api/Program.cs
@@ -18,8 +18,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: CorsPolicy,
         policy  =>
         {
-            policy.WithOrigins("http://localhost:3000",
-                "https://localhost:3000");
+            policy.WithOrigins(
+                    "http://localhost:3000",
+                    "https://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
         });
 });
 
