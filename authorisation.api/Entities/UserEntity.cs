c22a50cddb2ab3a4a937796438b770b3a11b3769
clew <lewis@clewup.co.uk>
2022-11-27 21:42:29 +0000

    Updated UserModel to not allow nullable types.

--- authorisation.api/Entities/UserEntity.cs
+++ authorisation.api/Entities/UserEntity.cs
@@ -8,11 +8,11 @@ public class UserEntity
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
    
-    public string? LineOne { get; set; }
-    public string? LineTwo { get; set; }
-    public string? LineThree { get; set; }
-    public string? Postcode { get; set; }
-    public string? City { get; set; }
-    public string? County { get; set; }
-    public string? Country { get; set; }
+    public string LineOne { get; set; } = "";
+    public string LineTwo { get; set; } = "";
+    public string LineThree { get; set; } = "";
+    public string Postcode { get; set; } = "";
+    public string City { get; set; } = "";
+    public string County { get; set; } = "";
+    public string Country { get; set; } = "";
 }
